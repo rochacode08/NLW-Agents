de99331d960fbc06d65b18e225090431ccba5350
Gabriel <gabriellops2443@gmail.com>
2025-07-12 20:21:36 -0500

    Estrutura inicial de manipulação de DOM

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Scrip Carregado")
+const apiKeyInput = document.getElementById("apiKey")
+const gameSelect = document.getElementById("gameSelect")
+const questionInput = document.getElementById("questionInput")
+const askButton = document.getElementById("askButton")
+const aiResponse = document.getElementById("aiResponse")
+const form = document.getElementById("form")
